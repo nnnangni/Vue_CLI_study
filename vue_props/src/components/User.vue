4489cefca0ab5d507100572786ce7c9f07422131
nnnangni <yud02150@gmail.com>
2019-07-26 16:47:42 +0900

    vue_props

---
+++ vue_props/src/components/User.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User 컴포넌트</h1>
+    <p>이름: {{ name }}</p>
+    <!-- <button @click="changeName()">이름 변경</button> -->
+    <v-btn @click="changeName()">이름 변경</v-btn>
+    <hr>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <!-- 자식 컴포넌트에 v-bind를 사용해서 데이터 전달 -->
+        <!-- 왼쪽에 자식에세 선언되는 props의 변수명, 오른쪽 변수에 어떤 값을 넣어줄 것인가 -->
+        <!-- 부모컴포넌트의 name이라는 값을 nameOfChild에 넣어줄게 -->
+        <UserDetail :nameOfChild="name"></UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue"
+import UserEdit from "./UserEdit.vue"
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit
+  },
+  data (){
+    return {
+      name:'뷰제이에스'
+    }
+  },
+  methods :{
+    changeName (){
+      this.name = "Nawon"
+    }
+  }
+}
+</script>
---
+++ vue_props/src/components/UserDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()">이름 변경</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  // 부모컴포넌트의 변수명 써주기 ['nameOfChild'] 처럼
+  // 부모 컴포넌트에서 이미 값을 가져왔기 때문에 값이 아닌 데이터 타입을 써준다
+  props:{
+    nameOfChild : {
+      type:String,
+      // 필요하다고 되어 있는데 부모에서 값을 넘겨주지 않으면 에러가 출력됨
+      required: true,
+      // 부모에서 아무값도 전달해주지 않았는데 default를 선언하면 그 값이 출력
+      // 만약 부모에서 값을 전달해주면 디폴트값 대신 부모에서 전달해준 값이 출력됨
+      default:'LEGO'
+  
+    }
+  },
+  computed:{
+    sayHello(){
+      return this.nameOfChild + '안녕하세요'
+    }
+  },
+  methods:{
+    switchName(){
+      this.nameOfChild = "컴퓨터"
+    }
+  }
+}
+</script>
